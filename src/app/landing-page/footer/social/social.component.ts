8bf4a845b4e6595858624d6cb05a65c6a7748b4c
Tonmoy Chiran <Tonmoy Chiran>
2020-11-02 11:58:00 +0600

    Social media links added

--- src/app/landing-page/footer/social/social.component.ts
+++ src/app/landing-page/footer/social/social.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social.component.scss']
 })
 export class SocialComponent implements OnInit {
-
+  socialMediaLinks=[{"link":"https://www.facebook.com/TheChiran/","image":"/assets/facebook.png"},
+                    {"link":"https://www.instagram.com/chiran_swe/","image":"/assets/instagram.png"},
+                    {"link":"https://www.linkedin.com/in/tonmoy-chiran-0920b2151/","image":"/assets/linkedin.png"}];
   constructor() { }
 
   ngOnInit(): void {
